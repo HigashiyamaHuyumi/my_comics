401e12bfe40552578cca2e93c24df8a6bc61ad06
HigasiyamaHuyumi <141430431+HigashiyamaHuyumi@users.noreply.github.com>
2024-01-25 14:00:39 +0000

    update DB

--- db/migrate/20231127080904_create_comic_volumes.rb
+++ db/migrate/20231127080904_create_comic_volumes.rb
@@ -1,8 +1,8 @@
 class CreateComicVolumes < ActiveRecord::Migration[6.1]
   def change
     create_table :comic_volumes do |t|
-      t.references :comic, foreign_key: true, null: false
-      t.references :volume, foreign_key: true, null: false
+      t.integer :comic_id, foreign_key: true, null: false
+      t.integer :volume_id, foreign_key: true, null: false
       t.timestamps
     end
   end
