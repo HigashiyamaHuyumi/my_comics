7f9ff7382dff9f4539b9e75a07d3e92a5fa15cf3
HigasiyamaHuyumi <141430431+HigashiyamaHuyumi@users.noreply.github.com>
2024-02-11 09:11:41 +0000

    update volumes_controller

--- app/controllers/user/volumes_controller.rb
+++ app/controllers/user/volumes_controller.rb
@@ -9,10 +9,10 @@ class User::VolumesController < ApplicationController
     # タグを削除できるのは、作成者のみ
     if @volume.user_id == current_user.id
       @volume.destroy
-      redirect_to tags_path, notice: '指定された巻数が削除されました。'
+      redirect_to volumes_path, notice: '指定された巻数が削除されました。'
     else
       flash[:alert] = '他のユーザーの巻数を削除することはできません。'
-      redirect_to tas_path
+      redirect_to volumes_path
     end
   end
 
